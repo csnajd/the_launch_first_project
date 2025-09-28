6444f6344a2daa1a77a0d5a2db82b13fefa4087c
najd <najdbina@gmail.com>
2025-09-28 14:46:38 +0300

    k

--- the_launch_first_project/ContentView.swift
+++ the_launch_first_project/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("hiiii")
+            Text("ho")
             Text("siii")
             
         }
