2d2d1e41ed09807416f5196388c27afcb55c5d67
Mayar <Mayar>
2025-10-01 14:50:13 +0300

    HomeView

--- the_launch_first_project/ContentView.swift
+++ the_launch_first_project/ContentView.swift
@@ -12,19 +12,59 @@ struct ContentView: View {
         ZStack {
             Color.background
                 .ignoresSafeArea()
-
-            VStack {
-                
-                HomeView()
-                AddPlayerView()
-                RoleView()
-                TimerView()
-                EndGameView()
-            }
-            .padding()
+            HomeView()
         }
+    }
+}
 
-     }
+struct HomeView: View {
+    @State private var logoOffset: CGFloat = 0
+    @State private var isButtonVisible = false
+    
+    var body: some View {
+        VStack {
+            // LOGO - يبقى ظاهراً دائماً
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 450, height: 450)
+                .offset(y: logoOffset)
+                .onAppear {
+                    withAnimation(.easeInOut(duration: 1)) {
+                        logoOffset = -200
+                    }
+                    
+                    // إظهار الزر بعد انتهاء حركة اللوقو
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        withAnimation {
+                            isButtonVisible = true
+                        }
+                    }
+                }
+                .padding(.top, 202)
+            
+            Spacer()
+            
+            if isButtonVisible {
+                Button(action: {
+                    print("العب")
+                }) {
+                    ZStack{
+                        Image("purpleBS")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 227, height: 55)
+                        
+                        Text("العب")
+                            .font(.MainText)
+                            .foregroundColor(.white)
+                    }
+                }
+                .transition(.move(edge: .bottom))
+                .padding(.bottom, 179)
+            }
+        }
+    }
 }
 
 #Preview {
